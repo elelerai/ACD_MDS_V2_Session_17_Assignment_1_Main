b57161c46952ead3e8b0fe1d747b63dc030701c3
elelerai <32876328+elelerai@users.noreply.github.com>
2018-11-25 11:48:09 -0800

    Add files via upload

---
+++ session_17_assignment_1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 25 10:42:31 2018
+
+@author: Eliud Lelerai
+"""
+import numpy as np
+import pandas as pd
+import scipy as ss
+from scipy import stats
+
+n = 20 # Number of questions
+p = 0.25  # There is only one correct answer in each of multiple choice questions
+q = 0.75  # The rest three are wrong in each of the questions
+# X=15  # number of questions answered correct
+# n-X=5   # number of questions answered wrong
+
+
+
+X = stats.binom(20, 0.25) # Declare X to be a binomial random variable
+
+#probability that a person undertaking that test has answered exactly 5 questions wrong.               
+ 
+               
+probability=1- X.pmf(15)           #1- P(X = 15)
+
+
+
+
+
+
